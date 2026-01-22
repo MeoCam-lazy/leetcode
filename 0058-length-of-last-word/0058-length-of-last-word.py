8bfd218932e2b27d1d5fa7d6a8ca3eecee9ec3ac
MeoCam-lazy <vankhoant01@gmail.com>
2026-01-22 22:40:37 +0700

    Time: 0 ms (100%), Space: 12.8 MB (7.59%) - LeetHub

---
+++ 0058-length-of-last-word/0058-length-of-last-word.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def lengthOfLastWord(self, s):
+        length = 0
+        for i in range(len(s)-1,-1,-1):
+            if(s[i] != ' '):
+                length += 1
+            elif length != 0:
+                return length
+        return length
+        
