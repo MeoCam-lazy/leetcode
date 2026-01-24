3bc9fa31e15189664c978b08a5c1ae7b12cd350d
MeoCam-lazy <vankhoant01@gmail.com>
2026-01-24 23:40:38 +0700

    Time: 1 ms (99.6%), Space: 80.8 MB (6.58%) - LeetHub

---
+++ 0053-maximum-subarray/0053-maximum-subarray.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int maxSubArray(int[] nums) {
+        int res = nums[0];
+        int total = 0;
+
+        for(int i = 0; i < nums.length; i++){
+            if(total < 0){
+                total = 0;
+            }
+            total += nums[i];
+            res = Math.max(res,total);
+        }
+        return res;
+    }
+}
