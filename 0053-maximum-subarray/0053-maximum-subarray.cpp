2fddeaeec6a1e56eb2f1978dc65622335bede442
MeoCam-lazy <vankhoant01@gmail.com>
2026-01-24 23:33:42 +0700

    Time: 0 ms (100%), Space: 71.9 MB (15.88%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.cpp
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -1,8 +1,7 @@
 class Solution {
 public:
     int maxSubArray(vector<int>& nums) {
-        int tempSum = 0;
-        int maxSum = INT_MIN;
+        int tempSum = 0, maxSum = INT_MIN;
         for(int i = 0; i < nums.size(); i++){
             tempSum = max(tempSum + nums[i], nums[i]);
             maxSum = max(maxSum,tempSum);
