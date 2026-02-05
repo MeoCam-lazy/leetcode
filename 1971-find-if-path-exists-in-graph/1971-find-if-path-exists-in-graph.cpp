6aaccb1f9d4994f9dfb79a4a025a07110594746f
MeoCam-lazy <vankhoant01@gmail.com>
2026-02-06 01:12:32 +0700

    Time: 1231 ms (5.48%), Space: 429.8 MB (5.02%) - LeetHub

--- 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.cpp
+++ 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.cpp
@@ -1,27 +1,23 @@
 class Solution {
 public:
     bool validPath(int n, vector<vector<int>>& edges, int source, int destination) {
-        if(n <= 1) return true;
-        unordered_map<int,vector<int>> graph;
-        for(auto edge: edges){
+        unordered_map<int,vector<int>> adjacency_list;
+        for(vector<int> edge: edges){
             int u = edge[0];
             int v = edge[1];
-            graph[v].push_back(u);
-            graph[u].push_back(v);
+            adjacency_list[u].push_back(v);
+            adjacency_list[v].push_back(u);
         }
-        unordered_set<int> visited;
-        return findPath(source,destination,graph,visited);
-    }
-    bool findPath(int current, int destination, unordered_map<int, vector<int>>& graph, unordered_set<int>& visited) {
-        visited.insert(current);
-        for (int neighbor : graph[current]) {
-            if (neighbor == destination) return true;
-            if (visited.find(neighbor) == visited.end()) {
-                if (findPath(neighbor, destination, graph, visited)) {
-                    return true;
-                }
-            }
-        }
+        set<int> visited;
+        dfs(adjacency_list,source,visited);
+        if(visited.find(destination) != visited.end()) return true;
         return false;
+    }
+    void dfs(unordered_map<int,vector<int>> &adjacency_list, int source, set<int>&visited){
+        visited.insert(source);
+        for(int x: adjacency_list[source]){
+            if(visited.find(x) == visited.end())
+            dfs(adjacency_list,x,visited);
+        }
     }
 };
