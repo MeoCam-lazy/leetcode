119ff3cefb98c5df93d3853b84dec12c15ba2114
MeoCam-lazy <vankhoant01@gmail.com>
2026-02-09 18:44:31 +0700

    Time: 0 ms (100%), Space: 16 MB (83.21%) - LeetHub

--- 0404-sum-of-left-leaves/0404-sum-of-left-leaves.cpp
+++ 0404-sum-of-left-leaves/0404-sum-of-left-leaves.cpp
@@ -16,6 +16,7 @@ public:
         dfs(root,sum,0);
         return sum;
     }
+    //It is given that we must return sum of every left leaves. So i just put a sign in to a recursive function with deapth first search whenever it's left branch. 
     void dfs(TreeNode*node, int &sum, int sign){
         if(node == nullptr) return;
         if(sign && node->left == nullptr && node->right == nullptr){
