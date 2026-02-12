5e62b6562cc2135302636ccec46a1e4f60ff94f1
MeoCam-lazy <vankhoant01@gmail.com>
2026-02-12 13:23:29 +0700

    Time: 3 ms (9.3%), Space: 9.3 MB (52.58%) - LeetHub

---
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int uniquePaths(int m, int n) {
+        vector<vector<int>> matrix(m,vector<int> (n,1));
+        for(int i = 1; i < m; i++){
+            for(int j = 1; j < n; j++){
+                matrix[i][j] = matrix[i][j-1] + matrix[i-1][j];
+            }
+        }
+        return matrix[m-1][n-1];
+    }
+};
