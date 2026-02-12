e716934be0f45734a5830e667cee899df6884f3d
MeoCam-lazy <vankhoant01@gmail.com>
2026-02-12 14:02:16 +0700

    Time: 12 ms (83.92%), Space: 13.8 MB (81.23%) - LeetHub

---
+++ 0064-minimum-path-sum/0064-minimum-path-sum.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def minPathSum(self, grid):
+        m = len(grid)
+        n = len(grid[0])
+        for i in range(1,n):
+            grid[0][i] = grid[0][i] + grid[0][i-1]
+        for j in range(1,m):
+            grid[j][0] = grid[j][0] + grid[j-1][0]
+        for i in range(1,m):
+            for j in range(1, n):
+                grid[i][j] += min(grid[i][j-1],grid[i-1][j])
+        return grid[m-1][n-1]
+        
