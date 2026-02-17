8ee77acc49a021f4c4629188c0bb1e47f83fb237
MeoCam-lazy <vankhoant01@gmail.com>
2026-02-17 23:20:59 +0700

    Time: 0 ms (100%), Space: 27.2 MB (36.44%) - LeetHub

---
+++ 0042-trapping-rain-water/0042-trapping-rain-water.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int trap(vector<int>& height) {
+        int n = height.size();
+        if (n == 0) return 0; 
+        vector<int> leftMax(n);
+        leftMax[0] = height[0];
+        for (int i = 1; i < n; i++) {
+            leftMax[i] = max(height[i], leftMax[i-1]);
+        }
+        vector<int> rightMax(n);
+        rightMax[n-1] = height[n-1];
+        for (int i = n - 2; i >= 0; i--) {
+            rightMax[i] = max(height[i], rightMax[i+1]);
+        }
+        int sum = 0;
+        for (int i = 0; i < n; i++) {
+            sum += min(leftMax[i], rightMax[i]) - height[i];
+        }
+        return sum;
+    }
+};
