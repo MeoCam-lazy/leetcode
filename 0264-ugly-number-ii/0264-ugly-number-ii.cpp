550475bddc6785d1e3f91e916629cab3af8b2169
MeoCam-lazy <vankhoant01@gmail.com>
2026-02-28 22:35:36 +0700

    Time: 7 ms (44.55%), Space: 8.4 MB (98.69%) - LeetHub

---
+++ 0264-ugly-number-ii/0264-ugly-number-ii.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int nthUglyNumber(int n) {
+        int dp[n+1];
+        dp[1] = 1;
+        int p2 = 1, p3 = 1, p5 = 1;
+        for(int i = 2; i <= n; i++){
+            int nextP2 = dp[p2]*2;
+            int nextP3 = dp[p3]*3;
+            int nextP5 = dp[p5]*5;
+            dp[i] = min({nextP2,nextP3,nextP5});
+            if(dp[i] == nextP2) p2++;
+            if(dp[i] == nextP3) p3++;
+            if(dp[i] == nextP5) p5++;
+        }
+        return dp[n];
+    }
+};
