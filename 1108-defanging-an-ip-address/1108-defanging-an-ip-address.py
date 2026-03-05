ccfddf05bc565828933f8253bd2e34cae3ed8e6e
MeoCam-lazy <vankhoant01@gmail.com>
2026-03-05 21:08:24 +0700

    Time: 15 ms (62.5%), Space: 12.5 MB (53.37%) - LeetHub

---
+++ 1108-defanging-an-ip-address/1108-defanging-an-ip-address.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def defangIPaddr(self, address):
+        result = ""
+        for char in address:
+            if char == '.':
+                result += '[.]'
+            else:
+                result += char
+        return result
+        
