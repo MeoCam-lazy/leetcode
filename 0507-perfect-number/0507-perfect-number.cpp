3e2d0d2ca449b97d73b77a6ce3803196db422224
MeoCam-lazy <vankhoant01@gmail.com>
2026-03-07 22:17:23 +0700

    Time: 2071 ms (32.05%), Space: 7.8 MB (55.06%) - LeetHub

---
+++ 0507-perfect-number/0507-perfect-number.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    bool checkPerfectNumber(int num) {
+        int total = 0;
+        for(int i = 1; i < num; i++){
+            if(num%i == 0) total += i; 
+        }
+        return total == num;
+    }
+    
+};
