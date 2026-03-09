fc4020d5d7c93e967de26d28553fe32cb795430b
MeoCam-lazy <vankhoant01@gmail.com>
2026-03-10 00:00:24 +0700

    Time: 19 ms (80.64%), Space: 18 MB (60.66%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -1,6 +1,13 @@
 class Solution {
 public:
     int coinChange(vector<int>& coins, int amount) {
+        /*@problem summary:
+        - You are given an integer array /coins/ representing coins of different denominations and an integer /amount/ 
+        - Your mission is to return the fewest number of coins that you need to make up that amount, note that assume you have countless amount of coins
+        @Approach:
+        It's easy to see that the minimum number of coins of any amount is the sum of minimum number of coins smaller than itself. Once we add 1 different denominations money, the minimum number of coin must be the sum of 1(itself) and sum_mini_amount[amount-that denominations]
+        */
+        
         vector<int> dp(amount + 1, amount + 1);
         dp[0] = 0;
 
