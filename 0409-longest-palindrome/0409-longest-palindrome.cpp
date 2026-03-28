86c5395978e53836e714cabc70979762852517ec
MeoCam-lazy <vankhoant01@gmail.com>
2026-03-28 22:28:26 +0700

    Time: 3 ms (16.65%), Space: 9.2 MB (60.88%) - LeetHub

--- 0409-longest-palindrome/0409-longest-palindrome.cpp
+++ 0409-longest-palindrome/0409-longest-palindrome.cpp
@@ -1,6 +1,12 @@
 class Solution {
 public:
     int longestPalindrome(string s) {
+        /*
+        @Problem summary:
+        - You are given an string S, return the longest palindrome can make from characters in s
+        @Apporach
+        - This is truly like a lego game. A palindrome is a string that reads the same forward and backward. We will count number of all disctinct characters in s, and add up the result whenever number of that character is divisable by 2, else if that number is not 1, then we can subtract it by 1 and then it will become an even number. And remember the mid number of the palindrome con sit alone so remember to add 1 whenever has odd number of char!
+        */
         int result = 0;
         map<char,int> tb;
         bool flag = false;
