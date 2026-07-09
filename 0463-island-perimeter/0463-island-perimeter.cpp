81d02dbf94f0c2bed410190926c9e35d4642df5b
MeoCam-lazy <vankhoant01@gmail.com>
2026-07-09 22:51:43 +0700

    Time: 0 ms (100%), Space: 102 MB (85.13%) - LeetHub

--- 0463-island-perimeter/0463-island-perimeter.cpp
+++ 0463-island-perimeter/0463-island-perimeter.cpp
@@ -3,6 +3,7 @@ public:
     int islandPerimeter(vector<vector<int>>& grid) {
         int land = 0;
         int connectedLand = 0;
+        /*The perimeter of the island is equal to number of landcells multiplied by 4, minus the number of shared borders multiplied by 2*/
         for(int i = 0; i < grid.size(); i++){
             for(int j = 0; j < grid[0].size(); j++){
                 if(grid[i][j] == 1){
